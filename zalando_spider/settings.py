a3cea4ac18a94b3811f07d46aad9f582e12a858f
elc <elc@atsg208.lss.emc.com>
2014-02-05 12:21:13 +0800

    Init

---
+++ zalando_spider/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for zalando_spider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'zalando_spider'
+
+SPIDER_MODULES = ['zalando_spider.spiders']
+NEWSPIDER_MODULE = 'zalando_spider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'zalando_spider (+http://www.yourdomain.com)'
